33676e865ede006a5a28cb2c0303906ab9c91b09
engus <micompany119@gmail.com>
2026-09-21 14:46:28 +0900

    fix: photoImageProvider 삼항연산자 타입추론 오류로 웹 빌드가 조용히 실패하던 문제 수정

--- lib/widgets/photo_image_native.dart
+++ lib/widgets/photo_image_native.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/widgets.dart';
 
 /// 'asset:'로 시작하면 pubspec에 번들된 이미지(데모 사진 등), 아니면 실제 파일 경로.
-ImageProvider photoImageProvider(String path) => path.startsWith('asset:')
-    ? AssetImage(path.substring('asset:'.length))
-    : FileImage(File(path));
+ImageProvider photoImageProvider(String path) {
+  if (path.startsWith('asset:')) return AssetImage(path.substring('asset:'.length));
+  return FileImage(File(path));
+}
--- lib/widgets/photo_image_stub.dart
+++ lib/widgets/photo_image_stub.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 
 /// 'asset:'로 시작하면 pubspec에 번들된 이미지(데모 사진 등).
 /// 그 외엔 image_picker가 돌려주는 blob: URL이라 NetworkImage로 바로 로드 가능.
-ImageProvider photoImageProvider(String path) => path.startsWith('asset:')
-    ? AssetImage(path.substring('asset:'.length))
-    : NetworkImage(path);
+ImageProvider photoImageProvider(String path) {
+  if (path.startsWith('asset:')) return AssetImage(path.substring('asset:'.length));
+  return NetworkImage(path);
+}
